afa7b019ba62bd39a15eaaec1bd562a8a02310fe
Saibabi <144018122+Saibabi@users.noreply.github.com>
2024-03-17 18:43:36 -0400

    Update main.dart
    add meal planner recipes and grocery list to the code while removing unneeded text

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_one/home_screen.dart';
 import 'package:project_one/recipe_list_screen.dart';
-import 'package:project_one/recipe_detail_screen.dart';
 import 'package:project_one/meal_planner_screen.dart';
 import 'package:project_one/grocery_list_screen.dart';
 
@@ -24,18 +23,6 @@ class MyApp extends StatelessWidget {
         '/mealplanner': (context) => MealPlannerScreen(),
         '/grocerylist': (context) => GroceryListScreen(),
       },
-      // Uncomment this section if you prefer using Navigator for navigation
-      // onGenerateRoute: (settings) {
-      //   switch (settings.name) {
-      //     case '/':
-      //       return MaterialPageRoute(builder: (context) => HomeScreen());
-      //     case '/recipes':
-      //       return MaterialPageRoute(builder: (context) => RecipeListScreen());
-      //     // You can add more cases for additional screens
-      //     default:
-      //       return MaterialPageRoute(builder: (context) => HomeScreen());
-      //   }
-      // },
     );
   }
 }
